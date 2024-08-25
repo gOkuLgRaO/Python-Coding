f0df9d437d72cb120cfdb4133557f3da94b57016
gOkuLgRaO <gokulrao10@gmail.com>
2024-08-25 20:41:51 +0530

    made changes and came to vscode

--- Django/mydemoproject/home/views.py
+++ Django/mydemoproject/home/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("Hey I am a Django server")
+    return HttpResponse("<h1>Hey I am a Django server</h1>")
+
+def success_page(request):
+    return HttpResponse("<h1> Hey this is a success page</h1>")
--- Django/mydemoproject/mydemoproject/urls.py
+++ Django/mydemoproject/mydemoproject/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from home.views import home
+from home.views import *
 
 urlpatterns = [
-    path('', home, name="home"),
+    path("", home, name="home"),
+    path("success-page/", success_page, name="success_page"),
     path("admin/", admin.site.urls),
 ]
