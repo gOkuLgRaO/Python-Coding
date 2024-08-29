08f0356156bfccf9ec94979177f3a6a2daa740e9
gOkuLgRaO <gokulrao10@gmail.com>
2024-08-29 13:47:38 +0530

    moved the problems folder into DSA folder

--- Django/mydemoproject/home/views.py
+++
@@ -1,9 +0,0 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def home(request):
-    return HttpResponse("<h1>Hey I am a Django server</h1>")
-
-def success_page(request):
-    return HttpResponse("<h1> Hey this is a success page</h1>")
